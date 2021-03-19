e199aee2ebd183131325fbbf64d939fbcc3ec0ae
Alyssa Winn <alyssawinn2@hotmail.com>
2021-03-19 00:11:40 -0600

    add inquire, fs, and update all functions

--- index.js
+++ index.js
@@ -1,13 +1,164 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+    
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            //Email
+            type: 'input',
+            name: 'email',
+            message: 'What is your email? (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email');
+                    return false;
+                }
+            }
+        },
+        {
+            //GitHub username
+            type: 'input',
+            name: 'username',
+            message: 'Enter your GitHub username: (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username');
+                    return false;
+                }
+            }
+        },
+        {
+            //Project Title
+            type: 'input',
+            name: 'title',
+            message: 'What is the name of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project title');
+                    return false;
+                }
+            }
+        },
+        {
+            //Description
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of your project: (Required)',
+            validate: descripInput => {
+                if (descripInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your project description');
+                    return false;
+                }
+            }
+        },
+        {
+            //Installation
+            type: 'input',
+            name: 'installation',
+            message: 'What are the steps required to install your project? (Required - Enter N/A if not applicable)',
+            validate: installInput => {
+                if (installInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your installation instructions.');
+                    return false;
+                }
+            }
+        },
+        {
+            //Usage
+            type: 'input',
+            name: 'usage',
+            message: 'Provide instructions for use: (Required - Enter N/A if not applicable)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your usage instructions.');
+                    return false;
+                }
+            }
+        },
+        {
+            //Licenses
+            type: 'list',
+            name: 'license',
+            message: 'Which license is needed for your project?',
+            choices: ['Apache', 'GNU GPLv3', 'ISC','MIT', 'No license needed']
+        },
+        {
+            //Contribution Guidelines
+            type: 'input',
+            name: 'contribute',
+            message: 'Provide guidelines for contributors: (Required - Enter N/A if not applicable)',
+            validate: contributeInput => {
+                if (contributeInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your contribution guidlines.');
+                    return false;
+                }
+            }
+        },
+        {
+            //Test instructions
+            type: 'input',
+            name: 'testing',
+            message: 'What are the steps to test? (Required - Enter N/A if not applicable)',
+            validate: testingInput => {
+                if (testingInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your steps to test.');
+                    return false;
+                }
+            }
+        }
+    ]);
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile('./README.md', data, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+    
+            resolve({
+                ok: true,
+                message: 'File created!'
+            });
+        });
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    questions()
+        .then(readmeData => {
+            console.log(readmeData);
+            return generateMarkdown(readmeData);
+        })
+        .then(pageXML => {
+            return writeToFile(pageXML);
+        })
+        .catch (err => {
+            console.log(err);
+        })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,98 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseBadge = "";
+  if (license === 'Apache') {
+    licenseBadge = "https://img.shields.io/badge/License-Apache%202.0-blue.svg";
+  }
+  else if (license === 'GNU GPLv3') {
+    licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg"
+  }
+  else if (license === 'ISC') {
+    licenseBadge = "https://img.shields.io/badge/License-ISC-blue.svg"
+  } 
+  else if (license === 'MIT') {
+    licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg"
+  }
+
+  return licenseBadge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink = "";
+  if (license === 'Apache') {
+    licenseLink = "https://opensource.org/licenses/Apache-2.0";
+  }
+  else if (license === 'GNU GPLv3') {
+    licenseLink = "https://www.gnu.org/licenses/gpl-3.0"
+  }
+  else if (license === 'ISC') {
+    licenseLink = "https://opensource.org/licenses/ISC"
+  } 
+  else if (license === 'MIT') {
+    licenseLink = "https://opensource.org/licenses/MIT"
+  }
+
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSec = "";
+  if (license === 'Apache') {
+    licenseSec = "This application is covered under the Apache license";
+  }
+  else if (license === 'GNU GPLv3') {
+    licenseSec = "This application is covered under the GNU GPLv3 license"
+  }
+  else if (license === 'ISC') {
+    licenseSec = "This application is covered under the ISC license"
+  } 
+  else if (license === 'MIT') {
+    licenseSec = "This application is covered under the MIT license"
+  }
+
+  return licenseSec;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-`;
+  [![License](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Contributing
+  ${data.contribute}
+
+  ## Tests
+  ${data.testing}
+
+  ## Questions
+  If you have any questions, please contact me at [${data.email}](mailto:${data.email}) or visit my GitHub page [here](https://github.com/${data.username}/)
+  `;
 }
 
 module.exports = generateMarkdown;
